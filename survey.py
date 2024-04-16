ba6d90619ef7e67b881afe54e5deaece40a8efba
Varun Murlidhar <vmurlidhar@unc.edu>
2024-04-15 21:20:09 -0400

    Complete survey questions and sharemaking

--- survey.py
+++ survey.py
@@ -41,8 +41,8 @@ name: str = input("What is your first name: ")
 
 gender: str = str(input("What gender do you identify with (female, male, nonbinary): "))
 
-#while not gender == "female" or not gender == "male" or not gender == "nonbinary":
-#    gender = input("What gender do you identify with (female, male, nonbinary): ")
+while gender != "female" and gender != "male" and gender != "nonbinary":
+    gender = input("What gender do you identify with (female, male, nonbinary): ")
 
 if gender == "female":  
     make_shares(1, f"{name}_female_1.txt", f"{name}_female_2.txt")
@@ -56,3 +56,54 @@ elif gender == "nonbinary":
     make_shares(0, f"{name}_female_1.txt", f"{name}_female_2.txt")
     make_shares(0, f"{name}_male_1.txt", f"{name}_male_2.txt")
     make_shares(1, f"{name}_nonbinary_1.txt", f"{name}_nonbinary_2.txt")
+
+
+comp_major: str = str(input("Are you a comp sci major? (Y/N) "))
+
+if comp_major == "Y":
+    make_shares(1, f"{name}_comp_major_1.txt", f"{name}_comp_major_2.txt")
+elif comp_major == "N":
+    make_shares(0, f"{name}_comp_major_1.txt", f"{name}_comp_major_2.txt")
+
+
+wellness: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how would you rate your wellness in general? "))
+
+while wellness < 1 or wellness > 5:
+    wellness: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how would you rate your wellness in general? "))
+
+make_shares(wellness, f"{name}_wellness_1.txt", f"{name}_wellness_2.txt")
+
+
+comp_comfortability: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how comfortable do you feel in your comp sci classes? "))
+while comp_comfortability < 1 or comp_comfortability > 5:
+    comp_comfortability: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how comfortable do you feel in your comp sci classes? "))
+
+make_shares(comp_comfortability, f"{name}_comp_comfortability_1.txt", f"{name}_comp_comfortability_2.txt")
+
+
+other_comfortability: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how comfortable do you feel in your other classes? "))
+while other_comfortability < 1 or other_comfortability > 5:
+    other_comfortability: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how comfortable do you feel in your other classes? "))
+
+make_shares(other_comfortability, f"{name}_other_comfortability_1.txt", f"{name}_other_comfortability_2.txt")
+
+
+course_performance: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how do you feel about your course performance? "))
+while course_performance < 1 or course_performance > 5:
+    course_performance: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how do you feel about your course performance? "))
+
+make_shares(course_performance, f"{name}_course_performance_1.txt", f"{name}_course_performance_2.txt")
+
+
+comp_professors: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how supported do you feel by your comp sci professors? "))
+while comp_professors < 1 or comp_professors > 5:
+    comp_professors: int = int(input("On a scale of 1-5 (1 = terrible, 5 = better than ever), how supported do you feel by your comp sci professors? "))
+
+make_shares(comp_professors, f"{name}_comp_professors_1.txt", f"{name}_comp_professors_2.txt")
+
+
+gpa: int = int(float(input("What is your GPA (Round to 2 decimals if necessary)? ")) * 100)
+while gpa < 0 or gpa > 400:
+    gpa: int = int(float(input("What is your GPA (Round to 2 decimals if necessary)? ")) * 100)
+
+make_shares(gpa, f"{name}_gpa_1.txt", f"{name}_gpa_2.txt")
